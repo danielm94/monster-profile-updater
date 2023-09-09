96e7d7d9c62de0718ce4f2e099f34a2afe179918
Daniel Martins <danielm94@users.noreply.github.com>
2023-09-09 16:03:21 -0400

    Initial commit of ResumeUpdateTest

---
+++ src/test/java/org/example/ResumeUpdateTest.java
@@ -0,0 +1,39 @@
+package org.example;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class ResumeUpdateTest {
+    private final static String MONSTER_URL = "https://www.monster.ca/";
+    private WebDriver driver;
+    private String username;
+    private String password;
+
+
+    @BeforeMethod
+    @Parameters({"username", "password"})
+    public void setUp(String username, String password){
+        this.username = username;
+        this.password = password;
+
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void testResumeUpdate(){
+        //-ea -Dusername=user -Dpassword=pass
+        driver.get(MONSTER_URL);
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        driver.quit();
+    }
+}
